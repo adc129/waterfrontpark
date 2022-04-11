fe31b4662d357f828f80b308652e570adf58d9b3
Aaron Carrasco <aaroncarr512@gmail.com>
2022-04-10 22:53:41 -0500

    add bootstrap

--- website/core/views.py
+++ website/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
+from .forms import PostForm
 
 
 class HomeView(ListView):
@@ -13,5 +14,6 @@ class BlogDetailView(DetailView):
 
 class AddBlogView(CreateView):
     model = Post
+    form_class = PostForm
     template_name = 'add_blog.html'
-    fields = '__all__'
+    #fields = ('title', 'body')
